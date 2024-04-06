eef00f910652e4cc90d8dcc0dd283c900f4f3654
Rovin Singh <rovinsingh990@gmail.com>
2024-04-06 19:39:45 +0530

    CSRF token

--- src/main/java/com/kick/it/kickit/filters/CsrfCookieFilter.java
+++ src/main/java/com/kick/it/kickit/filters/CsrfCookieFilter.java
@@ -4,20 +4,27 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 public class CsrfCookieFilter extends OncePerRequestFilter {
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-
-        System.out.println(csrfToken.getToken());
-//        csrfToken.getToken();
+        System.out.println("csrfToken.getToken()12"+csrfToken.getToken());
+        if(csrfToken.getToken()=="undefined"){
+            CookieCsrfTokenRepository.withHttpOnlyFalse().generateToken(request);
+            System.out.println("csrfToken.getToken()1");
+        }
         if(csrfToken!=null){
             response.setHeader(csrfToken.getHeaderName(), csrfToken.getToken());
+            System.out.println(csrfToken.getToken());
+            System.out.println("response.getHeader(csrfToken.getHeaderName())"+response.getHeader(csrfToken.getHeaderName()));
+            System.out.println("csrfToken.getHeaderName()"+csrfToken.getHeaderName());
         }
         System.out.println("CSRF filter");
         filterChain.doFilter(request,response);
